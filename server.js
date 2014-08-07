9512330a748f879e5256f02ed82ca03a624ffb21
Nick Raienko <enaqxx@gmail.com>
2014-08-07 08:12:28 +0300

    Update server

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+var comments = [{author: 'Author', text: 'Comment'}];
+
 app.use('/', express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/comments.json', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(comments));
+});
+
+app.post('/comments.json', function(req, res) {
+  comments.push(req.body);
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(comments));
+});
 
 app.listen(3000);
 
